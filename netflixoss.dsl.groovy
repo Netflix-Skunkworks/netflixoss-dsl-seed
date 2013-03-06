cdf3148ec2fa2cceefd16035d9f68465c6924316
Justin Ryan <jryan@netflix.com>
2013-03-05 23:25:09 -0800

    Rough draft of what it could look like

---
+++ netflixoss.dsl.groovy
@@ -0,0 +1,50 @@
+
+import groovy.json.*
+
+def repos = new JsonSlurper().parseText("https://api.github.com/orgs/Netflix/repos".toURL().text)
+def projectWhitelist = ['RxJava']
+repos.findAll { projectWhitelist.contains(it.name) }.each { repo ->
+    println "$repo.name $repo.url"
+    // Trunk build
+    job {
+        name "${repo.name}-master"
+        description ellipsize(repo.description, 255)
+        logRotator(60,-1,-1,20)
+        scm {
+            git(repo.git_url, 'master')
+        }
+        jdk('Sun JDK 1.6 (latest)')
+        configure { node ->
+            node / 'com.cloudbees.jenkins.GitHubPushTrigger'(plugin:'github@1.5') / spec
+        }
+        steps {
+            gradle('clean build')
+        }
+    }
+
+    // Pull request build
+    // Should be similar to above, but it's probably more readable to just repeat the code.
+    job {
+        name "${repo.name}-pullrequest"
+        description ellipsize(repo.description, 255)
+        logRotator(60,-1,-1,20)
+        scm {
+            git(repo.git_url, 'master')
+        }
+        jdk('Sun JDK 1.6 (latest)')
+        configure { node ->
+            node / 'com.cloudbees.jenkins.GitHubPushTrigger'(plugin:'github@1.5') / spec
+        }
+        // TBD CloudBees pull request plugin
+        steps {
+            gradle('clean test')
+        }
+    }
+}
+
+String ellipsize(String input, int maxLength) {
+  if (input == null || input.length() < maxLength) {
+    return input;
+  }
+  return input.substring(0, maxLength) + "...";
+}
--- run_dsl.sh
+++ run_dsl.sh
@@ -1,2 +1,2 @@
-DSL_JAR=$JOB_DSL_HOME/job-dsl-core/build/libs/job-dsl-core-1.13-SNAPSHOT-standalone.jar
-java -jar $DSL_JAR netflixoss.dsl.groovy
+DSL_JAR=$JOB_DSL_HOME/job-dsl-core/build/libs/job-dsl-core-*-SNAPSHOT-standalone.jar
+java -jar $DSL_JAR netflixoss.dsl.groovy $@
