bb936cf924a68405f5ec9b6aaacbc74bc7d5a1dd
Gareth Bowles <gbowles@netflix.com>
2013-03-07 12:24:29 -0800

    Final version

--- netflixoss.dsl.groovy
+++ netflixoss.dsl.groovy
@@ -7,7 +7,7 @@ repos.findAll { projectWhitelist.contains(it.name) }.each { repo ->
     println "$repo.name $repo.url"
     // Trunk build
     job {
-        name "${repo.name}-master"
+        name "test-${repo.name}-master"
         description ellipsize(repo.description, 255)
         logRotator(60,-1,-1,20)
         scm {
@@ -15,13 +15,13 @@ repos.findAll { projectWhitelist.contains(it.name) }.each { repo ->
         }
         jdk('Sun JDK 1.6 (latest)')
         configure { project ->
-            project / triggers / 'com.cloudbees.jenkins.GitHubPushTrigger'(plugin:"github@1.5") / spec {
+            project / triggers / 'com.cloudbees.jenkins.GitHubPushTrigger'(plugin:'github@1.5') / spec {
             }
         }
         configure { project ->
-            project / 'properties' / 'nectar.plugins.rbac.groups.JobProxyGroupContainer'(plugin:"nectar-rbac@3.4") / groups {
+            project / 'properties' / 'nectar.plugins.rbac.groups.JobProxyGroupContainer'(plugin:'nectar-rbac@3.4') / groups {
             }
-            project / 'properties' / 'com.cloudbees.jenkins.plugins.PublicKey'(plugin:"cloudbees-public-key@1.1") {
+            project / 'properties' / 'com.cloudbees.jenkins.plugins.PublicKey'(plugin:'cloudbees-public-key@1.1') {
             }
         }
         steps {
@@ -35,7 +35,7 @@ repos.findAll { projectWhitelist.contains(it.name) }.each { repo ->
     // Pull request build
     // Should be similar to above, but it's probably more readable to just repeat the code.
     job {
-        name "${repo.name}-pullrequest"
+        name "test-${repo.name}-pullrequest"
         description ellipsize(repo.description, 255)
         logRotator(60,-1,-1,20)
         scm {
@@ -43,19 +43,19 @@ repos.findAll { projectWhitelist.contains(it.name) }.each { repo ->
         }
         jdk('Sun JDK 1.6 (latest)')
         configure { project -> 
-            project / triggers / 'com.cloudbees.jenkins.GitHubPushTrigger'(plugin:"github@1.5") / spec {
+            project / triggers / 'com.cloudbees.jenkins.plugins.github__pull.PullRequestBuildTrigger'(plugin:'github-pull-request-build@1.0-beta-2') / spec {
             }
         }
         configure { project ->
-            project / 'properties' / 'nectar.plugins.rbac.groups.JobProxyGroupContainer'(plugin:"nectar-rbac@3.4") / groups {
+            project / 'properties' / 'nectar.plugins.rbac.groups.JobProxyGroupContainer'(plugin:'nectar-rbac@3.4') / groups {
             }
-            project / 'properties' / 'com.cloudbees.jenkins.plugins.PublicKey'(plugin:"cloudbees-public-key@1.1") {
+            project / 'properties' / 'com.cloudbees.jenkins.plugins.PublicKey'(plugin:'cloudbees-public-key@1.1') {
             }
-            project / 'properties' / 'com.cloudbees.jenkins.plugins.git.vmerge.JobPropertyImpl'(plugin:"git-validated-merge@3.6") / postBuildPushFailureHandler(class:"com.cloudbees.jenkins.plugins.git.vmerge.pbph.PushFailureIsFailure") 
-            project / 'properties' / 'com.coravy.hudson.plugins.github.GithubProjectProperty'(plugin:"github@1.5") / projectUrl('https://github.com/Netflix/RxJava/')
+            project / 'properties' / 'com.cloudbees.jenkins.plugins.git.vmerge.JobPropertyImpl'(plugin:'git-validated-merge@3.6') / postBuildPushFailureHandler(class:'com.cloudbees.jenkins.plugins.git.vmerge.pbph.PushFailureIsFailure') 
+            project / 'properties' / 'com.coravy.hudson.plugins.github.GithubProjectProperty'(plugin:'github@1.5') / projectUrl('https://github.com/Netflix/RxJava/')
         }
         steps {
-            gradle('clean test')
+            gradle('clean build')
         }
         publishers {
             archiveJunit('**/build/test-results/TEST*.xml')
@@ -67,5 +67,5 @@ String ellipsize(String input, int maxLength) {
   if (input == null || input.length() < maxLength) {
     return input;
   }
-  return input.substring(0, maxLength) + "...";
+  return input.substring(0, maxLength) + '...';
 }
